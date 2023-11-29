a5eb47eeb4c0d289a3adaff2de93df3ff951f0ad
Grzegorz Sikora <grzegorz.sikora@lyondellbasell.com>
2023-11-29 20:37:35 +0100

    add virtual scroll

--- src/components/TripsList.tsx
+++ src/components/TripsList.tsx
@@ -1,31 +1,45 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import { TripCard } from "./TripCard";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { Trip } from "../backend/backendTypes";
 import axios from "axios";
 
 export const TripsList = () => {
   const [trips, setTrips] = useState<Trip[] | null>(null);
+  const [display, setDisplay] = useState(10);
+  const [loading, setLoading] = useState(false);
+
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastTripElementRef = useCallback(
+    (node: HTMLElement | null) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setLoading(true);
+          setTimeout(() => {
+            setDisplay((prevDisplayNumber) => prevDisplayNumber + 10);
+            setLoading(false);
+          }, 500);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading]
+  );
 
   useEffect(() => {
     const fetchTrips = async () => {
+      setLoading(true);
       try {
-        let response;
-        const cachedResponse = localStorage.getItem("trips");
-
-        if (cachedResponse) {
-          response = JSON.parse(cachedResponse);
-          setTrips(response);
-        } else {
-          response = await axios.get<Trip[]>("http://localhost:3001/trips");
-          localStorage.setItem("trips", JSON.stringify(response.data));
-          setTrips(response.data);
-        }
+        const response = await axios.get<Trip[]>("http://localhost:3001/trips");
+        setTrips(response.data);
       } catch (error) {
         console.error("Error:", error);
+      } finally {
+        setLoading(false);
       }
     };
-
     fetchTrips();
   }, []);
 
@@ -40,8 +54,16 @@ export const TripsList = () => {
         paddingTop={"3rem"}
       >
         {trips &&
-          trips.map((trip) => {
-            return <TripCard {...trip} key={trip.id} />;
+          trips.slice(0, display).map((trip, index) => {
+            if (trips.slice(0, display).length === index + 1) {
+              return (
+                <div ref={lastTripElementRef} key={trip.id}>
+                  <TripCard {...trip} />
+                </div>
+              );
+            } else {
+              return <TripCard {...trip} key={trip.id} />;
+            }
           })}
       </Box>
     </ChakraProvider>
